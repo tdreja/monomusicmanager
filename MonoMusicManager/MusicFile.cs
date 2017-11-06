885836d939b6ee7caa7d86ebb624b586dd3dbcab
Thomas Dreja <thomas.dreja@uni-ulm.de>
2017-11-06 11:05:43 +0100

    Fixed Destination: Now the destination file is correctly generated

--- MonoMusicManager/MusicFile.cs
+++ MonoMusicManager/MusicFile.cs
@@ -1,62 +1,170 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace MonoMusicManager
 {
     class MusicFile
     {
-        public string artist { get; protected set; }
-        public string title { get; protected set; }
-        public string album { get; protected set; }
-        public string genre { get; protected set; }
-        public string path { get; protected set; }
+        public string Artist { get; protected set; }
+        public string Title { get; protected set; }
+        public string Album { get; protected set; }
+        public string Genre { get; protected set; }
+        public string Source { get; protected set; }
 
-        public uint trackNr { get; protected set; }
-        public uint discNr { get; protected set; }
+        public uint TrackNr { get; protected set; }
+        public uint DiscNr { get; protected set; }
 
-        public MusicFolder.Folders folder;
-        public string albumParentFolder;
-        public bool variousArtists;
+        public MusicFolder.Folders Folder { get; internal set; }
+
+        public string AlbumParentFolder { get; internal set; }
+        public bool HasVariousArtists { get; internal set; }
+        public uint MaxDiscNr { get; internal set; }
+        public uint MaxTrackNr { get; internal set; }
 
         public MusicFile(string path)
         {
             FileInfo info = new System.IO.FileInfo(path);
+            Source = info.FullName;
             if(info.Exists)
             {
-                this.path = info.FullName;
-                TagLib.File file = TagLib.File.Create(this.path);
+                TagLib.File file = TagLib.File.Create(this.Source);
 
-                artist = file.Tag.FirstPerformer;
-                title = file.Tag.Title;
-                album = file.Tag.Album;
-                genre = file.Tag.FirstGenre;
-                trackNr = file.Tag.Track;
-                discNr = file.Tag.Disc;
-            } else
+                Artist = file.Tag.FirstPerformer;
+                Title = file.Tag.Title;
+                Album = file.Tag.Album;
+                Genre = file.Tag.FirstGenre;
+                TrackNr = file.Tag.Track;
+                DiscNr = file.Tag.Disc;
+            }
+            else
             {
-                Console.WriteLine("No Exist " + path);
+                Artist = null;
+                Title = null;
+                Album = null;
+                Genre = null;
+                TrackNr = 0;
+                DiscNr = 0;
             }
 
-            folder = MusicFolder.Folders.NONE;
-            albumParentFolder = null;
-            variousArtists = false;
+            Folder = MusicFolder.Folders.NONE;
+            AlbumParentFolder = null;
+            HasVariousArtists = false;
+            MaxDiscNr = 0;
+            MaxTrackNr = 0;
         }
 
         public MusicFile(string artist, string title, string album, string genre, string path)
         {
-            this.artist = artist;
-            this.title = title;
-            this.album = album;
-            this.path = path;
-            this.genre = genre;
-            trackNr = 1;
-            discNr = 1;
-            folder = MusicFolder.Folders.NONE;
-            albumParentFolder = null;
-            variousArtists = false;
+            Artist = artist;
+            Title = title;
+            Album = album;
+            Source = path;
+            Genre = genre;
+            TrackNr = 1;
+            DiscNr = 1;
+            Folder = MusicFolder.Folders.NONE;
+            AlbumParentFolder = null;
+            HasVariousArtists = false;
+            MaxDiscNr = 0;
+            MaxTrackNr = 0;
+        }
+
+        public string GetDestination(string targetFolder)
+        {
+            FileInfo sourceFile = new FileInfo(Source);
+            string newPath = MusicFolder.GetPath(targetFolder, Folder);
+            //Console.WriteLine("Source " + sourceFile + " new " + newPath);
+
+            if (Folder == MusicFolder.Folders.ALBUM || Folder == MusicFolder.Folders.FILMMUSIK 
+                || Folder == MusicFolder.Folders.PODCASTS || Folder == MusicFolder.Folders.LIEDER)
+            {
+                if (AlbumParentFolder != null && AlbumParentFolder.Length > 0)
+                {
+                    newPath = Path.Combine(newPath, AlbumParentFolder);
+                }
+
+                //Console.WriteLine("Folder: "+ CreateFolderName() + " File: " + CreateFileName(sourceFile, true));
+
+                return Path.Combine(newPath, CreateFolderName(), CreateFileName(sourceFile, true));
+            }
+            else
+            {
+                return Source;
+            }
+        }
+
+        public bool IsValid()
+        {
+            return Title != null && Title.Length > 0 && Artist != null && Artist.Length > 0;
+        }
+
+        public bool CanCopy()
+        {
+            return IsValid() && Folder != MusicFolder.Folders.NONE && Folder != MusicFolder.Folders.PLAYLISTS;
+        }
+
+        private string CreateFolderName()
+        {
+            if(Artist != null)
+            {
+                if (Folder == MusicFolder.Folders.LIEDER || Album == null || Album.Length == 0)
+                {
+                    return Artist;
+                }
+                else if (HasVariousArtists)
+                {
+                    return Album;
+                }
+                else
+                {
+                    return Artist + " - " + Album;
+                }
+            }
+            else
+            {
+                return "";
+            }
+            
+        }
+
+        private string CreateFileName(FileInfo sourceFile, bool useTrackNr)
+        {
+            if(useTrackNr && TrackNr > 0)
+            {
+                return FormatDiscNr() + FormatTrackNr() + " - " + Title + sourceFile.Extension;
+            }
+            else
+            {
+                return Title + sourceFile.Extension;
+            }
+        }
+
+        public string FormatTrackNr()
+        {
+            if(MaxTrackNr >= 100)
+            {
+                return TrackNr.ToString("000");
+            }
+            else
+            {
+                return TrackNr.ToString("00");
+            }
+        }
+
+        public string FormatDiscNr()
+        {
+            uint disc = Math.Max(1, DiscNr);
+
+            if(MaxDiscNr >= 10)
+            {
+                return disc.ToString("00");
+            }
+            else
+            {
+                return disc.ToString("0");
+            }
         }
     }
 
@@ -98,7 +206,7 @@ namespace MonoMusicManager
             }
         }
 
-        public static List<MusicFile> sortFiles(string targetRoot, params MusicFile[] unsortedFiles)
+        public static List<MusicFile> SortFiles(string targetFolder, params MusicFile[] unsortedFiles)
         {
             List<MusicFile> sortedFiles = new List<MusicFile>();
 
@@ -107,20 +215,30 @@ namespace MonoMusicManager
             // Group in albums and check how many songs are in each album
             foreach(MusicFile file in unsortedFiles)
             {
-                if(file.album != null)
+                if(file.Album != null)
                 {
-                    if(albumInfos.ContainsKey(file.album))
+                    if(albumInfos.ContainsKey(file.Album))
                     {
-                        AlbumInfo info = albumInfos[file.album];
-                        info.songCount++;
-                        if(!info.various && !info.artist.Equals(file.artist))
+                        AlbumInfo info = albumInfos[file.Album];
+                        info.SongCount++;
+                        if(!info.IsVarious && !info.Artist.Equals(file.Artist))
                         {
-                            info.various = true;
+                            info.IsVarious = true;
+                        }
+
+                        if(info.MaxDisc <= file.DiscNr)
+                        {
+                            info.MaxDisc = file.DiscNr;
+                        }
+
+                        if(info.MaxTrack <= file.TrackNr)
+                        {
+                            info.MaxTrack = file.TrackNr;
                         }
                     }
                     else
                     {
-                        albumInfos.Add(file.album, new AlbumInfo(file.artist));
+                        albumInfos.Add(file.Album, new AlbumInfo(file.Artist, file.DiscNr, file.TrackNr));
                     }
                 }
             }
@@ -129,51 +247,53 @@ namespace MonoMusicManager
 
             foreach(MusicFile file in unsortedFiles)
             {
-                if(file.album != null)
+                if(file.Album != null)
                 {
-                    if(albumInfos.ContainsKey(file.album))
+                    if(albumInfos.ContainsKey(file.Album))
                     {
-                        file.variousArtists = albumInfos[file.album].various;
+                        file.HasVariousArtists = albumInfos[file.Album].IsVarious;
+                        file.MaxDiscNr = albumInfos[file.Album].MaxDisc;
+                        file.MaxTrackNr = albumInfos[file.Album].MaxTrack;
                     }
 
-                    if(albumInfos.ContainsKey(file.album) && albumInfos[file.album].songCount >= MINIMUM_ALBUM_SONG_NUMBER)
+                    if(albumInfos.ContainsKey(file.Album) && albumInfos[file.Album].SongCount >= MINIMUM_ALBUM_SONG_NUMBER)
                     {
-                        if(file.genre != null && file.genre.Contains("Soundtrack"))
+                        if(file.Genre != null && file.Genre.Contains("Soundtrack"))
                         {
-                            file.folder = Folders.FILMMUSIK;
+                            file.Folder = Folders.FILMMUSIK;
                         }
                          else
                         {
-                            file.folder = Folders.ALBUM;
+                            file.Folder = Folders.ALBUM;
                         }
 
-                        if(parentFolder.ContainsKey(file.album))
+                        if(parentFolder.ContainsKey(file.Album))
                         {
-                            file.albumParentFolder = parentFolder[file.album];
+                            file.AlbumParentFolder = parentFolder[file.Album];
                         }
                         else
                         {
-                            file.albumParentFolder = checkParentFolder(albumInfos[file.album].artist, file.variousArtists, file.album, file.folder, targetRoot);
-                            parentFolder.Add(file.album, file.albumParentFolder);
+                            file.AlbumParentFolder = CheckParentFolder(albumInfos[file.Album].Artist, file.HasVariousArtists, file.Album, file.Folder, targetFolder);
+                            parentFolder.Add(file.Album, file.AlbumParentFolder);
                         }
                     }
                     else
                     {
-                        file.folder = Folders.LIEDER;
+                        file.Folder = Folders.LIEDER;
                     }
                 }
                 else
                 {
-                    file.folder = Folders.LIEDER;
+                    file.Folder = Folders.LIEDER;
                 }
-
+                
                 sortedFiles.Add(file);
             }
 
             return sortedFiles;
         }
 
-        private static string checkParentFolder(string artist, bool various, string album, Folders folder, string targetRoot)
+        private static string CheckParentFolder(string artist, bool various, string album, Folders folder, string targetRoot)
         {
             string mainPath = GetPath(targetRoot, folder);
 
@@ -212,15 +332,19 @@ namespace MonoMusicManager
 
     class AlbumInfo
     {
-        public int songCount;
-        public string artist;
-        public bool various;
+        public int SongCount { get; internal set; }
+        public uint MaxDisc { get; internal set; }
+        public uint MaxTrack { get; internal set; }
+        public string Artist { get; internal set; }
+        public bool IsVarious { get; internal set; }
 
-        public AlbumInfo(string artist)
+        public AlbumInfo(string artist, uint discNr, uint trackNr)
         {
-            this.artist = artist;
-            various = false;
-            songCount = 1;
+            this.Artist = artist;
+            IsVarious = false;
+            SongCount = 1;
+            MaxDisc = discNr;
+            MaxTrack = trackNr;
         }
     }
 
--- MonoMusicManager/Program.cs
+++ MonoMusicManager/Program.cs
@@ -19,17 +19,19 @@ namespace MonoMusicManager
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            string prefix = "F:\\Musik\\Album\\Hardstyle\\Hardstyle Vol 11";
+            string musicFolder = "C:\\Users\\thoma\\Downloads\\etc";
+            string prefix = Path.Combine(musicFolder, "Apocalyptica - Worlds Collide");
             //string prefix = "F:\\Downloads\\Musik\\Solaris";
 
-            MusicFile file = new MusicFile(Path.Combine(prefix, "103 - Fahrenheit.mp3"));
+            MusicFile file = new MusicFile(Path.Combine(prefix, "101 - Worlds Collide.mp3"));
             //MusicFile file = new MusicFile("F:\\Musik\\Lieder\\311\\Love Song.mp3");
-            Console.WriteLine("Title " + file.title + " Artist " + file.artist + " Album " + file.album + " Genre "+ file.genre + " Path " + file.path + " Track "+file.trackNr + " Disc "+file.discNr);
 
-            List<MusicFile> sorted = MusicFolder.sortFiles("F:\\Musik\\", file, new MusicFile(Path.Combine(prefix, "110 - Back Again.mp3")), new MusicFile(Path.Combine(prefix, "114 - Magma.mp3")));
+            List<MusicFile> sorted = MusicFolder.SortFiles("F:\\Musik\\", file, new MusicFile(Path.Combine(prefix, "105 - Helden.mp3")), new MusicFile(Path.Combine(prefix, "109 - Burn.mp3")));
             foreach(MusicFile tmp in sorted)
             {
-                Console.WriteLine("Parent " + tmp.albumParentFolder + " Various "+ tmp.variousArtists + " Folder " + tmp.folder);
+                Console.WriteLine("Info " + tmp.Title + " " + tmp.DiscNr + " " + tmp.TrackNr);
+                //Console.WriteLine("Parent " + tmp.AlbumParentFolder + " Various "+ tmp.HasVariousArtists + " Folder " + tmp.Folder);
+                Console.WriteLine("New Target " + tmp.GetDestination("F:\\"));
             }
         }
     }
